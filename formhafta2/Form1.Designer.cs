0dcc429a01ece0a1bb090efc4b3922faceffa7d3
Entersoft4 <alperen7434@gmail.com>
2023-10-28 12:47:30 +0300

    UpdateV1.3

--- formhafta2/Form1.Designer.cs
+++ formhafta2/Form1.Designer.cs
@@ -274,7 +274,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(748, 329);
+            this.ClientSize = new System.Drawing.Size(743, 329);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lbox1);
             this.Controls.Add(this.l9);
